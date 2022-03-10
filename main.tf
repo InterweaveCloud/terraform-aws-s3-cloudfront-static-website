cad642097c328b3076963a20bc42a378970e30dc
Vic <vichassan9@gmail.com>
2022-03-10 16:28:44 +0000

    error testing

--- main.tf
+++ main.tf
@@ -5,13 +5,21 @@ resource "aws_s3_bucket" "website_files" {
   bucket = lower("${var.bucket_prefix}-${data.aws_caller_identity.current.account_id}")
 
   force_destroy = "true"
-
-
+  
+  #acl = "private"
   #versioning {
-   # enabled = "true"
+    #enabled = "true"
   #}
 }
+
 resource "aws_s3_bucket_acl" "website_files" {
   bucket = aws_s3_bucket.website_files.id
   acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "website_files" {
+  bucket = aws_s3_bucket.website_files.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
