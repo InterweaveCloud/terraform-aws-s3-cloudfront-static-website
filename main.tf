c5160d4741f20ff95bc354045cf84418c0a18a68
Vic <vichassan9@gmail.com>
2022-03-10 16:17:17 +0000

    error testing

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ resource "aws_s3_bucket" "website_files" {
 
   bucket = lower("${var.bucket_prefix}-${data.aws_caller_identity.current.account_id}")
 
-  #acl           = "private"
-  #force_destroy = "true"
+  acl           = "private"
+  force_destroy = "true"
 
 
   #versioning {
