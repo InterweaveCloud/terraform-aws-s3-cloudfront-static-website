daf1718e0f2dbf96c2f6be37e3721eebff5e19ff
Vic <vichassan9@gmail.com>
2022-03-10 15:33:16 +0000

    Commented content for module test

--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@ resource "aws_s3_bucket" "website_files" {
 
   bucket = lower("DONtest-website-files-${data.aws_caller_identity.current.account_id}")
 
-  acl           = "private"
-  force_destroy = "true"
+  #acl           = "private"
+  #force_destroy = "true"
 
 
-  versioning {
-    enabled = "true"
-  }
+  #versioning {
+   # enabled = "true"
+  #}
 }
