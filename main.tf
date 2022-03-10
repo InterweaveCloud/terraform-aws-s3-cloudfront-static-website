4c7add384a7bac5917bdb482fe8ff42f26f986c3
Vic <vichassan9@gmail.com>
2022-03-10 16:08:15 +0000

    Introduced bucket-prefix variable

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "website_files" {
 
-  bucket = lower("DONtest-website-files-${data.aws_caller_identity.current.account_id}")
+  bucket = lower("${var.bucket_prefix}-${data.aws_caller_identity.current.account_id}")
 
   #acl           = "private"
   #force_destroy = "true"
