2503a239d0a23b5764566f68e0d91f3006a7266f
Vic <vichassan9@gmail.com>
2022-03-11 13:34:00 +0000

    Variable objects - values defined through module

--- main.tf
+++ main.tf
@@ -1,15 +1,11 @@
+#AWS user ID
 data "aws_caller_identity" "current" {}
-
+#S3 bucket to host all website files.
 resource "aws_s3_bucket" "website_files" {
 
   bucket = lower("${var.bucket_prefix}-${data.aws_caller_identity.current.account_id}")
 
   force_destroy = "true"
-  
-  #acl = "private"
-  #versioning {
-    #enabled = "true"
-  #}
 }
 
 resource "aws_s3_bucket_acl" "website_files" {
--- variables.tf
+++ variables.tf
@@ -1,4 +1,30 @@
+# Variable access file - values entrered through module
 variable "bucket_prefix" {
   type = string
   description = "The name of the bucket without the www. prefix. Normally domain_name."
 }
+
+variable "domain_name" {
+  type = string
+  description = "The domain name for the website."
+}
+
+variable "s3_origin_id" {
+  type = string
+  description = "Origin name for the S3's CloudFront distribution"
+}
+
+variable "cloudfront_oai" {
+  type = string
+  description = "Origin access identity name"
+}
+
+variable "hosted_zone_id" {
+  type = string
+  description = "ID for the Hosted zone that handles DNS for TCP and UDP traffic"
+}
+
+# variable "domain_name" {
+#   type = string
+#   description = ""
+# }
