ff0c3d7a311d1310135bdab400bb6d99e7a01d32
Vic <vichassan9@gmail.com>
2022-03-11 13:43:04 +0000

    Cloudfront distribution for non-www s3 site. HTTP requests automatically redirected to HTTPS. Cache behaviour, custom error message, acm certificates, ttl values are also all defined for the non-www s3 site.

--- main.tf
+++ main.tf
@@ -112,3 +112,92 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
 }
+
+# Cloudfront distribution for non-www. s3 site.
+resource "aws_cloudfront_distribution" "root_distribution" {
+
+  provider = aws.useast1
+
+  origin {
+    domain_name = aws_s3_bucket.website_files.bucket_regional_domain_name
+    origin_id   = var.s3_origin_id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.cloudfront_oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "${var.bucket_prefix}-CloudfrontDistribution"
+  default_root_object = "index.html"
+
+# Default cache behaviour
+  default_cache_behavior {
+
+    allowed_methods  = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = var.s3_origin_id
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/content/immutable/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = var.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+
+  }
+
+  custom_error_response {
+    error_caching_min_ttl = 60
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/"
+  }
+
+# non-www site
+  aliases = ["${var.domain_name}"]
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate_validation.example.certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.1_2016"
+  }
+
+}
