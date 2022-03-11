9dac6bd4a359dfb811e1b7997ebebf9f7ad157da
Vic <vichassan9@gmail.com>
2022-03-11 13:51:46 +0000

    Route 53 - DNS Handling for TCP & UDP requests. Hosted zone id is uniquely generated per domain.

--- main.tf
+++ main.tf
@@ -246,3 +246,52 @@ resource "aws_acm_certificate" "ssl_certificate" {
     create_before_destroy = true
   }
 }
+
+# Route 53 - DNS Handling for TCP & UDP requests
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.ssl_certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = var.hosted_zone_id
+}
+
+resource "aws_acm_certificate_validation" "example" {
+  provider = aws.useast1
+
+  certificate_arn         = aws_acm_certificate.ssl_certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
+
+resource "aws_route53_record" "www-a" {
+  zone_id = var.hosted_zone_id
+  name    = "www.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "root-a" {
+  zone_id = var.hosted_zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.root_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.root_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
