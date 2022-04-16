189db3d3f08e866e2376e8a715096ddf174906e4
Faizan Raza <faizan.raza@infinityworks.com>
2022-04-16 18:18:56 +0100

    Functioning SSL Certificate

--- main.tf
+++ main.tf
@@ -25,118 +25,162 @@ resource "aws_s3_bucket" "website_files" {
   tags          = local.tags
 }
 
-// resource "aws_s3_bucket_acl" "website_files" {
-//   bucket = aws_s3_bucket.website_files.id
-//   acl    = "private"
-// }
+# Private S3 bucket preferred for security
+resource "aws_s3_bucket_acl" "website_files" {
+  bucket = aws_s3_bucket.website_files.id
+  acl    = "private"
+}
 
-// resource "aws_s3_bucket_versioning" "website_files" {
-//   bucket = aws_s3_bucket.website_files.id
-//   versioning_configuration {
-//     status = "Enabled"
-//   }
-// }
+# Versioning enabled to allow recovery of accidentally over written files
+resource "aws_s3_bucket_versioning" "website_files" {
+  bucket = aws_s3_bucket.website_files.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
-// #-----------------------------------------------------------------------------------------------
-// # Cloudfront distribution for main www.s3 site. HTTP requests automatically redirected to HTTPS.
-// #-----------------------------------------------------------------------------------------------
-// resource "aws_cloudfront_origin_access_identity" "cloudfront_oai" {
-//   comment = var.cloudfront_oai
-// }
+#-----------------------------------------------------------------------------------------------
+# Create SSL Certificate, route53 records for validation and then validate the certificate
+#-----------------------------------------------------------------------------------------------
+resource "aws_acm_certificate" "ssl_certificate" {
+  provider = aws.useast1
 
-// resource "aws_cloudfront_distribution" "s3_distribution" {
+  domain_name = var.domain_name
 
-//   provider = aws.useast1
+  # DNS validation requires the domain nameservers to already be pointing to AWS
+  validation_method         = "DNS"
+  subject_alternative_names = ["*.${var.domain_name}"]
+  tags                      = local.tags
 
-//   origin {
-//     domain_name = aws_s3_bucket.website_files.bucket_regional_domain_name
-//     origin_id   = var.s3_origin_id
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-//     s3_origin_config {
-//       origin_access_identity = aws_cloudfront_origin_access_identity.cloudfront_oai.cloudfront_access_identity_path
-//     }
-//   }
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.ssl_certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
-//   enabled             = true
-//   is_ipv6_enabled     = true
-//   comment             = "${var.bucket_prefix}-CloudfrontDistribution"
-//   default_root_object = "index.html"
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = var.hosted_zone_id
+}
 
-// # Default cache behaviour
-//   default_cache_behavior {
+resource "aws_acm_certificate_validation" "ssl_certificate_validation" {
+  provider = aws.useast1
 
-//     allowed_methods  = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
-//     cached_methods   = ["GET", "HEAD", "OPTIONS"]
-//     target_origin_id = var.s3_origin_id
-//     forwarded_values {
-//       query_string = false
+  certificate_arn         = aws_acm_certificate.ssl_certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
 
-//       cookies {
-//         forward = "none"
-//       }
-//     }
+# -----------------------------------------------------------------------------------------------
+# Cloudfront distribution for main www.s3 site. HTTP requests automatically redirected to HTTPS.
+# -----------------------------------------------------------------------------------------------
+# resource "aws_cloudfront_origin_access_identity" "cloudfront_oai" {
+#   comment = var.cloudfront_oai
+# }
 
-//     viewer_protocol_policy = "redirect-to-https"
-//     min_ttl                = 0
-//     default_ttl            = 3600
-//     max_ttl                = 86400
-//   }
+# resource "aws_cloudfront_distribution" "s3_distribution" {
 
-//   # Cache behavior with precedence 0 - takes priority over the Default cache behaviour
-//   ordered_cache_behavior {
-//     path_pattern     = "/content/immutable/*"
-//     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-//     cached_methods   = ["GET", "HEAD", "OPTIONS"]
-//     target_origin_id = var.s3_origin_id
+#   provider = aws.useast1
 
-//     forwarded_values {
-//       query_string = false
-//       headers      = ["Origin"]
+#   origin {
+#     domain_name = aws_s3_bucket.website_files.bucket_regional_domain_name
+#     origin_id   = var.s3_origin_id
 
-//       cookies {
-//         forward = "none"
-//       }
-//     }
+#     s3_origin_config {
+#       origin_access_identity = aws_cloudfront_origin_access_identity.cloudfront_oai.cloudfront_access_identity_path
+#     }
+#   }
 
-// # TTL value for the period of time that a packet/data should exist before being discarded.
-//     min_ttl                = 0
-//     default_ttl            = 86400
-//     max_ttl                = 31536000
-//     compress               = true
-//     viewer_protocol_policy = "redirect-to-https"
-//   }
+#   enabled             = true
+#   is_ipv6_enabled     = true
+#   comment             = "${var.bucket_prefix}-CloudfrontDistribution"
+#   default_root_object = "index.html"
 
-//   price_class = "PriceClass_200"
+#   # Default cache behaviour
+#   default_cache_behavior {
 
-//   restrictions {
-//     geo_restriction {
-//       restriction_type = "none"
-//     }
+#     allowed_methods  = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+#     cached_methods   = ["GET", "HEAD", "OPTIONS"]
+#     target_origin_id = var.s3_origin_id
+#     forwarded_values {
+#       query_string = false
 
-//   }
+#       cookies {
+#         forward = "none"
+#       }
+#     }
 
-// # Custom error messages
-//   custom_error_response {
-//     error_caching_min_ttl = 60
-//     error_code            = 403
-//     response_code         = 200
-//     response_page_path    = "/"
-//   }
+#     viewer_protocol_policy = "redirect-to-https"
+#     min_ttl                = 0
+#     default_ttl            = 3600
+#     max_ttl                = 86400
+#   }
 
-//   aliases = ["www.${var.domain_name}"] # www. site
+#   # Cache behavior with precedence 0 - takes priority over the Default cache behaviour
+#   ordered_cache_behavior {
+#     path_pattern     = "/content/immutable/*"
+#     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+#     cached_methods   = ["GET", "HEAD", "OPTIONS"]
+#     target_origin_id = var.s3_origin_id
 
-//   viewer_certificate {
-//     acm_certificate_arn      = aws_acm_certificate_validation.example.certificate_arn
-//     ssl_support_method       = "sni-only"
-//     minimum_protocol_version = "TLSv1.1_2016"
-//   }
+#     forwarded_values {
+#       query_string = false
+#       headers      = ["Origin"]
 
-// }
+#       cookies {
+#         forward = "none"
+#       }
+#     }
 
-// #-----------------------------------------------------------------------------------------------
-// # Cloudfront distribution for non-www. s3 site.
-// #-----------------------------------------------------------------------------------------------
-// resource "aws_cloudfront_distribution" "root_distribution" {
+#     # TTL value for the period of time that a packet/data should exist before being discarded.
+#     min_ttl                = 0
+#     default_ttl            = 86400
+#     max_ttl                = 31536000
+#     compress               = true
+#     viewer_protocol_policy = "redirect-to-https"
+#   }
+
+#   price_class = "PriceClass_200"
+
+#   restrictions {
+#     geo_restriction {
+#       restriction_type = "none"
+#     }
+
+#   }
+
+#   # Custom error messages
+#   custom_error_response {
+#     error_caching_min_ttl = 60
+#     error_code            = 403
+#     response_code         = 200
+#     response_page_path    = "/"
+#   }
+
+#   aliases = ["www.${var.domain_name}"] # www. site
+
+#   viewer_certificate {
+#     acm_certificate_arn      = aws_acm_certificate_validation.example.certificate_arn
+#     ssl_support_method       = "sni-only"
+#     minimum_protocol_version = "TLSv1.1_2016"
+#   }
+
+# }
+
+#-----------------------------------------------------------------------------------------------
+# Cloudfront distribution for non-www. s3 site.
+#-----------------------------------------------------------------------------------------------
+#  resource "aws_cloudfront_distribution" "root_distribution" {
 
 //   provider = aws.useast1
 
@@ -252,48 +296,9 @@ resource "aws_s3_bucket" "website_files" {
 //   policy = local.cloudfront_website_bucket_access
 // }
 
-// #-----------------------------------------------------------------------------------------------
-// # SSL Certificate with 'DNS' validation method
-// #-----------------------------------------------------------------------------------------------
-// resource "aws_acm_certificate" "ssl_certificate" {
-//   provider = aws.useast1
-
-//   domain_name       = var.domain_name
-
-//   # DNS validation requires the domain nameservers to already be pointing to AWS
-//   validation_method = "DNS"
-
-//   subject_alternative_names = ["*.${var.domain_name}"]
 
 
-//   tags = {
-//     Name = "${var.bucket_prefix}-cert"
-//   }
 
-//   lifecycle {
-//     create_before_destroy = true
-//   }
-// }
-
-// #-----------------------------------------------------------------------------------------------
-// # Route 53 - DNS Handling for TCP & UDP requests
-// #-----------------------------------------------------------------------------------------------
-// resource "aws_route53_record" "cert_validation" {
-//   for_each = {
-//     for dvo in aws_acm_certificate.ssl_certificate.domain_validation_options : dvo.domain_name => {
-//       name   = dvo.resource_record_name
-//       record = dvo.resource_record_value
-//       type   = dvo.resource_record_type
-//     }
-//   }
-
-//   allow_overwrite = true
-//   name            = each.value.name
-//   records         = [each.value.record]
-//   ttl             = 60
-//   type            = each.value.type
-//   zone_id         = var.hosted_zone_id
-// }
 
 // #-----------------------------------------------------------------------------------------------
 // #ACM certificate
--- versions.tf
+++ versions.tf
@@ -1,8 +1,9 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "4.10.0"
+      source                = "hashicorp/aws"
+      version               = "4.10.0"
+      configuration_aliases = [aws.useast1]
     }
   }
 }
