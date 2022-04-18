0e7fafb7fd255469a18b4c3c74e3268f4e816069
Faizan Raza <faizan.raza@infinityworks.com>
2022-04-18 15:15:16 +0100

    added s3 bucket access policy

--- outputs.tf
+++ outputs.tf
@@ -25,8 +25,18 @@ output "s3_bucket_acl" {
 output "s3_bucket_versioning" {
   description = "The bucket versioning status."
   value       = var.bucket_versioning
-
 }
+
+output "s3_bucket_access_policy_json" {
+  description = "JSON bucket policy to allow CloudFront to access the S3 bucket."
+  value       = local.cloudfront_website_bucket_access
+}
+
+output "s3_bucket_access_policy" {
+  description = "Bucket policy to allow CloudFront to access the S3 bucket."
+  value       = jsondecode(local.cloudfront_website_bucket_access)
+}
+
 
 #------------------------------------------------------------------------------
 # SSL Certificate
@@ -85,3 +95,68 @@ output "cloudfront_origin_access_identity_s3_canonical_user_id" {
   description = "The Amazon S3 canonical user ID for the origin access identity, which you use when giving the origin access identity read permission to an object in Amazon S3."
   value       = aws_cloudfront_origin_access_identity.cloudfront_oai.s3_canonical_user_id
 }
+
+#------------------------------------------------------------------------------
+# CloudFront Distribution
+#------------------------------------------------------------------------------
+
+output "cloudfront_distribution_id" {
+  description = "The identifier for the distribution. For example: EDFDVBD632BHDS5."
+  value       = aws_cloudfront_distribution.s3_distribution.id
+}
+
+output "cloudfront_distribution_arn" {
+  description = "The ARN (Amazon Resource Name) for the distribution. For example: arn:aws:cloudfront::123456789012:distribution/EDFDVBD632BHDS5, where 123456789012 is your AWS account ID."
+  value       = aws_cloudfront_distribution.s3_distribution.arn
+}
+
+output "cloudfront_distribution_caller_reference" {
+  description = "Internal value used by CloudFront to allow future updates to the distribution configuration."
+  value       = aws_cloudfront_distribution.s3_distribution.caller_reference
+}
+
+output "cloudfront_distribution_status" {
+  description = "The current status of the distribution. Deployed if the distribution's information is fully propagated throughout the Amazon CloudFront system."
+  value       = aws_cloudfront_distribution.s3_distribution.status
+}
+
+output "cloudfront_distribution_tags_all" {
+  description = "A map of tags assigned to the resource, including those inherited from the provider default_tags configuration block."
+  value       = aws_cloudfront_distribution.s3_distribution.tags_all
+}
+
+output "cloudfront_distribution_trusted_key_groups" {
+  description = "List of nested attributes for active trusted key groups, if the distribution is set up to serve private content with signed URLs"
+  value       = aws_cloudfront_distribution.s3_distribution.trusted_key_groups
+}
+
+output "cloudfront_distribution_trusted_signers" {
+  description = "List of nested attributes for active trusted signers, if the distribution is set up to serve private content with signed URLs"
+  value       = aws_cloudfront_distribution.s3_distribution.trusted_signers
+}
+
+output "cloudfront_distribution_domain_name" {
+  description = "The domain name corresponding to the distribution. For example: d604721fxaaqy9.cloudfront.net."
+  value       = aws_cloudfront_distribution.s3_distribution.domain_name
+}
+
+output "cloudfront_distribution_last_modified_time" {
+  description = "The date and time the distribution was last modified."
+  value       = aws_cloudfront_distribution.s3_distribution.last_modified_time
+}
+
+output "cloudfront_distribution_in_progress_validation_batches" {
+  description = "The number of invalidation batches currently in progress."
+  value       = aws_cloudfront_distribution.s3_distribution.in_progress_validation_batches
+}
+
+output "cloudfront_distribution_etag" {
+  description = "The current version of the distribution's information. For example: E2QWRUHAPOMQZL."
+  value       = aws_cloudfront_distribution.s3_distribution.etag
+}
+
+output "cloudfront_distribution_hosted_zone_id" {
+  description = "The CloudFront Route 53 zone ID that can be used to route an Alias Resource Record Set to. This attribute is simply an alias for the zone ID Z2FDTNDATAQYW2."
+  value       = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+}
+
